bb9de0cd1bd8ca1e7fe5fc7acb0d39f8a65ef06f
OleksiiFL <oleksii.rudenko.fl@gmail.com>
2024-02-24 14:49:33 +0200

    dumpling were changed for star dust. Some other corrections.

--- javaCore4/src/Main.java
+++ javaCore4/src/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
             int toadEyesCount = 0;
             int ravenBonesCount = 0;
             int ghoulTearsCount = 0;
-            int dumplingsCount = 0;
+            int starDustCount = 0;
 
             System.out.println("\t\tRecipe book of Tamriel.");
-            System.out.println("Eaglesight elixir: 3 toad eyes + 1 raven bone.");
-            System.out.println("Stamina elixir: 2 raven bones + 4 dumplings");
-            System.out.println("Invisibility elixir: 7 ghoul tears + 1 dumpling");
-            System.out.println("Forbidden elixir: 5 ghoul tears + 10 dumplings + 4 toad eyes + 3 raven bones");
+            System.out.println("Eaglesight elixir: 3  toad eyes + 1 raven bone.");
+            System.out.println("Stamina elixir: 2 raven bones + 4 star dust.");
+            System.out.println("Invisibility elixir: 7 ghoul tears + 1 star dust + 2 toad eyes.");
+            System.out.println("Forbidden elixir: 5 ghoul tears + 10 star dust + 4 toad eyes + 3 raven bones.");
             System.out.println("You can make your elixir. Use 1 for toad eyes, 2 for ghoul tears," +
-                    "3 for raven bones and 4 for dumplings. To mix reagents press other key.");
+                    " 3 for raven bones and 4 for star dust. To mix reagents press other key.");
 
             for(; true; ) {
                 int aInput = new Scanner(System.in).nextInt();
@@ -27,8 +27,8 @@ import java.util.Scanner;
                     ravenBonesCount++;
                     System.out.println("You added " + ravenBonesCount + " raven bones.");
                 } else if(aInput == 4) {
-                    dumplingsCount++;
-                    System.out.println("You added " + dumplingsCount + " dumplings.");
+                    starDustCount++;
+                    System.out.println("You added " + starDustCount + " dumplings.");
                 } else {
                     break;
                 }
@@ -36,11 +36,11 @@ import java.util.Scanner;
 
             if(toadEyesCount == 3 && ravenBonesCount == 1) {
                 System.out.println("You've created the Eaglesight elixir!");
-            } else if(ravenBonesCount == 2 && dumplingsCount ==4) {
+            } else if(ravenBonesCount == 2 && starDustCount ==4) {
                 System.out.println("You've created the Stamina elixir!");
-            } else if(ghoulTearsCount == 7 && dumplingsCount == 1 && toadEyesCount == 2) {
+            } else if(ghoulTearsCount == 7 && starDustCount == 1 && toadEyesCount == 2) {
                 System.out.println("You've created the Invisibility elixir!");
-            } else if(ghoulTearsCount == 10 && dumplingsCount == 4 && toadEyesCount == 4 && ravenBonesCount == 3) {
+            } else if(ghoulTearsCount == 10 && starDustCount == 4 && toadEyesCount == 4 && ravenBonesCount == 3) {
                 System.out.println("You've created the Forbidden elixir!");
             } else {
                 System.out.println("Urgh! It smells so nasty!");
