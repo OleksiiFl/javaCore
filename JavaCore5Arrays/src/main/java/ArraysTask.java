ddccaeee9de2e05e01e9a5a7f301cd7377a4820c
OleksiiFL <oleksii.rudenko.fl@gmail.com>
2024-02-25 16:50:04 +0200

    start with arrays. Random Additional task: [0] = 10, [1] = 9......

--- JavaCore5Arrays/src/main/java/ArraysTask.java
+++ JavaCore5Arrays/src/main/java/ArraysTask.java
@@ -3,16 +3,21 @@ import java.util.Random;
 public class ArraysTask {
     public static void main(String[] args) {
 
-        int[] array = new int[10];
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
         for (int i = 0; i < array.length; i++) {
-            array[i] = new Random().nextInt(10);
             System.out.println(array[i]);
         }
         System.out.println("From back to beginning:");
-        for (int i = array.length - 1; i > -1; i = i - 1) {
+        for (int i = array.length - 1; i >= 0; i--) {
             System.out.println(array[i]);
         }
+        System.out.println("---------------------");
+        for (int i = 0; i < array.length; i++) {
+            int a = array.length - i;
+            array[i] = a;
+            System.out.println(array[i]);
+        }
+        System.out.println("sss " + array[2]);
 
     }
-
 }
