0b07376735d3a19fdfc6254df880b4733d0fe43a
OleksiiFL <oleksii.rudenko.fl@gmail.com>
2024-02-26 09:49:10 +0200

    start with arrays. Random Additional task: [0] = 10, [1] = 9...... other way of coding for the add task

--- JavaCore5Arrays/src/main/java/Arrays.java
+++ JavaCore5Arrays/src/main/java/Arrays.java
@@ -14,5 +14,13 @@ public class Arrays {
             summ += bankAge[i];
         }
         System.out.println(summ / bankAge.length);
+
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i < array.length; i++) {
+            int a = array.length - i;
+            array[i] = a;
+            System.out.println(array[i]);
+        }
+        System.out.println("sss " + array[2]);
     }
 }
