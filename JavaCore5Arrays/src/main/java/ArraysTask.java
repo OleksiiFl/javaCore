b39584e3c598af3cc78154cfca6d9ecb05de636f
OleksiiFL <oleksii.rudenko.fl@gmail.com>
2024-02-26 09:42:32 +0200

    start with arrays. Random Additional task: [0] = 10, [1] = 9...... other way of coding for the add task

--- JavaCore5Arrays/src/main/java/ArraysTask.java
+++ JavaCore5Arrays/src/main/java/ArraysTask.java
@@ -3,21 +3,26 @@ import java.util.Random;
 public class ArraysTask {
     public static void main(String[] args) {
 
+//        int[] array = {1,2,3,4,5,6,7,8,9,10};
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//        }
+//        System.out.println("From back to beginning:");
+//        for (int i = array.length - 1; i >= 0; i--) {
+//            System.out.println(array[i]);
+//        }
+//        System.out.println("---------------------");
+//        int[] array = new int[10];
         int[] array = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i < array.length / 2; i++) {
+//            array[i] = new Random().nextInt(20);
+            int a = array[i];
+            array[i] = array[array.length - (1 + i)];
+            array[array.length - (1 + i)] = a;
+        }
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
-        System.out.println("From back to beginning:");
-        for (int i = array.length - 1; i >= 0; i--) {
-            System.out.println(array[i]);
-        }
-        System.out.println("---------------------");
-        for (int i = 0; i < array.length; i++) {
-            int a = array.length - i;
-            array[i] = a;
-            System.out.println(array[i]);
-        }
-        System.out.println("sss " + array[2]);
 
     }
 }
