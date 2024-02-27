9cd0bc688d3504ad3a191d5c71012e172fa3f833
OleksiiFL <oleksii.rudenko.fl@gmail.com>
2024-02-27 17:06:28 +0200

    once again, branches...

---
+++ NewTestGit/src/main/java/FirstArray.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class FirstArray {
+    public static void main(String[] args) {
+        int[] bankAge = new int[10];
+
+        for (int i = 0; i < bankAge.length; i++) {
+            bankAge[i] = new Random().nextInt(100);
+            System.out.println(bankAge[i]);
+        }
+        double summ = 0;
+        for (int i = 0; i < bankAge.length; i++) {
+            summ += bankAge[i];
+        }
+        System.out.println(summ / bankAge.length);
+    }
+}
