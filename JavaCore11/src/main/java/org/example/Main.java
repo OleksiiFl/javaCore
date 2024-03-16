acdfd8d70679e1f5dba8e81d305437e5508d1b0d
JavaVioliNina <niag024@ukr.net>
2024-03-16 22:41:16 +0200

    Codewars task, new method for sum of all digits while there are > 1 of them.

--- JavaCore11/src/main/java/org/example/Main.java
+++ JavaCore11/src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Enter number:");
         int input = new Scanner(System.in).nextInt();
         System.out.println("Average of all digits is " + avg(input));
+        System.out.println("Sum of all digits is " + digital_root(input));
 
     }
     public static double avg(int number) {
@@ -16,5 +17,20 @@ public class Main {
             sum += Integer.parseInt(String.valueOf(inputString.charAt(i)));
         }
         return sum / inputString.length();
+    }
+    public static int digital_root(int n) {
+        String input = String.valueOf(n);
+        int sum = 0;
+        for(int i = 0; i < input.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(input.charAt(i)));
+        }
+        while(sum > 9) {
+            String sumStr = String.valueOf(sum);
+            sum = 0;
+            for(int i = 0; i < sumStr.length(); i++) {
+                sum += Integer.parseInt(String.valueOf(sumStr.charAt(i)));
+            }
+        }
+        return sum;
     }
 }
