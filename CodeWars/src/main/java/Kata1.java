e8e3fbcfd0b526396bbdb394c1404aa711e11654
JavaVioliNina <niag024@ukr.net>
2024-03-25 22:48:22 +0200

    trying to find out solutions for kata with lack of knowledge..

--- CodeWars/src/main/java/Kata1.java
+++ CodeWars/src/main/java/Kata1.java
@@ -1,15 +1,20 @@
 public class Kata1 {
+    public static final String SPACE = "\\s";
+    public static final String NON_SPACE = "\\S";
+
     public static void main(String[] args) {
-        System.out.println(solution("treasure"));
+        System.out.println(solution("  wef  fqef"));
     }
-    public static String solution(String str) {
-        String result = "";
-        for(int i = 1; i <= str.length(); i++) {
-            result += str.charAt(str.length()-i);
+        public static String solution (String str){
+            String result = "";
+            for (int i = 1; i <= str.length(); i++) {
+                result += str.charAt(str.length() - i);
+            }
+            return result;
         }
-        return result;
-    }
+
+
     /*public static String solutionTop(String str) {
         return new StringBuilder(str).reverse().toString();
     }*/
-}
+    }
