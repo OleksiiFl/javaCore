e2aa735f71c9e694c22d2bbe61e044de078a6d82
OleksiiFl <oleksii.rudenko.fl@gmail.com>
2024-04-27 13:30:40 +0300

    Sorted results

--- CompetitionCalculator/src/main/java/Main.java
+++ CompetitionCalculator/src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
     public static Map<String, Integer> competitorsList = new LinkedHashMap<>();
+    public static int juryNumber = 0;
     public static void main(String[] args) {
         competitorsList.put("Софія Пірожик", 0);
         competitorsList.put("Анастасія Гаврилюк", 0);
@@ -11,6 +12,9 @@ public class Main {
         competitorsList.put("Христина Скуратовець", 0);
         competitorsList.put("Софія Зейдліц", 0);
         competitorsList.put("Валерія Романченко", 0);
+
+        System.out.println("Вітаємо в програмі підрахунку результатів оцінювання конкурсу! Введіть кількість членів жюрі:");
+        juryNumber = new Scanner(System.in).nextInt();
         while(true){
             System.out.println("Щоб додати бали введіть ім'я учасника та оцінки " +
                     "(кожну оцінку окремо, для переходу до іншого учасника натисніть 1). " +
@@ -28,20 +32,37 @@ public class Main {
 
         }
         printResult();
-        sortedArray();
+        resultInTreeMap();
+        //sortedArray();
     }
     public static void putValueToMap(String key,  int score) {
         competitorsList.put(key, competitorsList.get(key) + score);
     }
     public static void printResult() {
+        System.out.println("Загальна кількість балів:");
         for (Map.Entry<String, Integer> resultsEntry : competitorsList.entrySet()) {
-            double score = resultsEntry.getValue();
-            System.out.println(resultsEntry.getKey() + " - " + score / 6);
+            System.out.println(resultsEntry.getKey() + " - " + resultsEntry.getValue());
         }
     }
+    public static void resultInTreeMap() {
+        System.out.println("\nРейтинг по балам (від останнього місця):");
+        competitorsList.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(System.out::println);
+        /*Map<String, Integer> rating = new TreeMap<>(competitorsList);
+        for(Map.Entry<String, Integer> ratingEntry : rating.entrySet()) {
+            double score = ratingEntry.getValue();
+            System.out.println(ratingEntry.getKey() + " - " + ratingEntry.getValue() / juryNumber);
+        }*/
+
+    }
     public static void sortedArray() {
+        System.out.println("Рейтинг по балам:");
         Set<Integer> finalScores = new TreeSet<>(competitorsList.values());
-        for(Integer i : finalScores) {
+        Integer[] arr = finalScores.toArray(new Integer[0]);
+        Arrays.sort(arr);
+        for(Integer i : arr) {
             System.out.println(i);
         }
     }
